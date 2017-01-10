01b8b52d6baa280dc43d8e4bc2ac3ad44a6c4a94
tgr0612 <tgr0612@naver.com>
2017-01-10 13:27:29 +0900

    소캣 생성 & 이벤트

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ var http = require('http');
 var fs = require('fs');
 var socket= require('socket.io');
 
-var server http.createServer(function(request, response){
+var server = http.createServer(function(request, response){
   fs.readFile('HTMLpage.htm', function(erro, data){
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end(data);
   });
 }).listen(3000, function(){
   console.log('Sever is running at 3000 port!');
+});
+
+var io = socketio.listen(server);
+io.sockets.on('connection', function(socket){
+
+  sockets.on('message', function(data){
+
+    io.sockets.emit('message',data);
+  });
 });
